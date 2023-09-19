5ad2408fd64e3ce8dd5991c16d5023cc36494226
Mahfouz98 <145352617+Mahfouz98@users.noreply.github.com>
2023-09-19 16:27:12 +0300

    Delete Test_0.py

--- Test_0.py
+++
@@ -1,6 +0,0 @@
-#Dictionary & split function
-
-userinput1=(input("hello man could you please tell us how long is your dick and your underwear size ?"))
-tallandsize=userinput1.split()
-tallandsizedic={"tall":tallandsize[0],"size":tallandsize[1]}
-print(tallandsizedic)
